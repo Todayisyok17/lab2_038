cc9d7f311bf4224911864f01ac0ff1b1622c8c83
todayis_yok <yog2546@gmail.com>
2024-11-21 17:28:34 +0700

    first commit

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,16 @@
+
+
+
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+    AppBar; AppBar(title: Text("652021038"),
+    backgroundColor:Colors.red,
+    
+  }
+}
